230d5f4c1a27dd571729425c99e4924def67fad8
yell-23 <yellwintzaw2302@gmail.com>
2023-11-10 17:11:49 +0630

    finished

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#F4F2EC",
+        secondaryColor: "#0E3746",
+        accentColor: "#6D1919",
+      },
+    },
+  },
+  plugins: [],
+};
